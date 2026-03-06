e1dd0135294f24853c222b781accd8dbe103af29
Nadim Mahmud <nadimmahmud.cs@gmail.com>
2026-03-06 01:52:42 -0500

    Updated scripts1

--- Scripts/surface_clustering_21_gpu.py
+++ Scripts/surface_clustering_21_gpu.py
@@ -53,7 +53,7 @@ CONFIG = {
     "pca_variance"       : 0.95,
 
     # raw windowed data
-    "windowed_csv"       : Path("../Datasets/ExtractedFeatures/labeled_accelerometer_windows.csv"),
+    "windowed_csv"       : Path("../Datasets/ExtractedFeatures/labeled_accelerometer_raw_windows.csv"),
 
     # VibClustNet hyper-parameters
     "vcn_epochs"         : 150,
